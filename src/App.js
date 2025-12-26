ee060e4a6a71e3d5e86a1c96e5edcaf958b34d28
ratheesh-srishtis <ratheeshsam.sics@gmail.com>
2025-12-26 12:48:09 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ const App = () => {
   return (
     <>
       <ErrorBoundary>
-        <Router basename="/">
+        <Router basename="">
           <MediaProvider>
             <AuthProvider>
               <Routes>
